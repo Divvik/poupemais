2593604fbb3efc532727df9ab1db4717c340ace9
Fernando Estevam <fernandoestevam23@gmail.com>
2019-02-15 16:44:36 -0200

    Ajustando erro no login

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -6,25 +6,26 @@ namespace App\Controllers;
 # use
 use Src\Core\Controllers;
 use Src\Core\Session;
-use Src\Core\Views;
 
 # Classe DashboardController
 class DashboardController extends Controllers
 {
     public function __construct()
-    {
+    {   
+        parent::__construct();
+        
         Session::init();
         $logged = Session::get('loggedIn');
        
         if($logged == false) {
             Session::destroy();
-            header('location:../login');
+            header('location: ../login');
             exit; 
-        }
+        } 
     }
     
     public function index()
-    {
+    {   
         $this->view->render('dashboard/index');
     }
 }
