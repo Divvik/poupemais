dc7dc2c59cb2e92f512bc3ef0ff2c7ba14c5d6ca
Fernando Estevam <fernandoestevam23@gmail.com>
2019-02-24 18:59:08 -0300

    Excluindo linha da index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 	header("Content-Type: text/html; charset=utf-8");
-	
 	# Declarando os requires dos arquivos necessarios para inicializacao
 	require_once 'src/vendor/autoload.php';
 	require_once 'config/config.php';
