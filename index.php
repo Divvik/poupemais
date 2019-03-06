8a9c362e4b8d3efb23cb0c771cc15f68120d9989
Cleyton Rodrigues <cleytonrs09@gmail.com>
2019-03-06 18:29:29 -0300

    novo layout
    Precisa da imagem do logo novo e da imagem de fundo para substituir no background.... As media queries nao estao funcionando muito bem

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
-<!DOCTYPE html>
 <html>
 <head>
 	<title>Poupe Mais</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 	<header id="menu">
 		<div id="menu_icon">
-			<i id="icone" class="fa fa-bars">
+			<img src="menu.png">
 				<a href="#menu_oculto"></a>	
-			</i>
 		</div>
 		<div id="left">
 			<img id="logo" src="logo poupe.png">
@@ -21,30 +16,22 @@
 		<div id="right">
 			<nav>
 				<ul id="menu_oculto">
-					<li><a href="#"><p>Home</p></a></li>
+					<li><a href="#"><p>Sobre nós</p></a></li>
+					<li><a href="#"><p>Simule</p></a></li>
 					<li><a href="#"><p>Investimentos</p></a></li>
-					<li><a href="#"><p>Contato</p></a></li>
-					<li><a href="#"><p>Sobre</p></a></li>
+					<li><a href="#"><p>Planos</p></a></li>
+					<li><a href="#"><p>Blog</p></a></li>
 					<li><a href="#"><span><p>Login</p></span></a></li>
 				</ul>
 			</nav>
 		</div>
 	</header>
 	<section id="Principal">
-		<img id="imgP" src="finance.jpg">
-		<!-- Direitos do freepik -->
-		<a href="https://br.freepik.com/fotos-vetores-gratis/fundo" style="color: white">Fundo vetor criado por rawpixel.com - br.freepik.com</a>
+		<aside id="Secundario">
+			<h1>Facilite seu dia, gaste seu tempo com o que realmente importa!</h1>
+			<br>
+			<h2><span><a href="#">Criar minha conta</a></span></h2>
+		</aside>
 	</section>
-	<aside id="Secundario">
-		<h1>Investir agora está ao seu alcance!</h1>
-		<br>
-		<h2>Conheça nossos planos!</h2>
-		<br>
-		<h3><span><a href="#">Quero saber mais!</a></span></h3>
-		<br>
-	</aside>
-	<footer>
-		<p>Poupe Mais &copy; Todos os direitos reservados</p>
-	</footer>
 </body>
 </html>
